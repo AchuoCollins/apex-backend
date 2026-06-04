dac782854e89337c2dca54ff928543146199150e
achuocollins <obenachuo@gmail.com>
2026-06-04 10:53:40 +0100

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -16,6 +16,8 @@ import app.models  # noqa: F401 — registers all models with metadata
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
+
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
